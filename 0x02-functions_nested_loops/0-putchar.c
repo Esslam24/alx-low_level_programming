e58582956b2f38ef84d340359404907da1d61afc
Esslam24 <islaaaaaaam1234@gmail.com>
2022-10-18 11:18:58 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- *  main - Prints "_putchar" followed by a new line.
- *
- *  Return: Always 0.
+ * maain - prints _putcfar and a new line
+ * Return: Always o
  */
 int main(void)
 {
