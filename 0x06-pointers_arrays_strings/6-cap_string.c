94e7fb61417d618f7856bd48b123bb449dff1eef
Esslam24 <islaaaaaaam1234@gmail.com>
2022-10-26 11:22:20 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,27 +6,34 @@
  *
  * Return: the resulting string
  */
-char *cap_string(char *s)
+char *cap_string(char *str)
 {
-	int i, j;
+	int index = 0;
 
-	char spe[13] = {' ', '\t', '\n', ',', ';', '.', 
-		'!', '?', '"', '(', ')', '{', '}'};
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
-			s[i] -= 32;
-
-		for (j = 0; j < 13; j++)
+	while (str[index])
 		{
-			if (s[i] == spe[j])
-			{
-				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
-				{
-					s[i + 1] -= 32;
-				}
-			}
+			while (!(str[index] >= 'a' && str[index] <= 'z'))
+				index++;
+
+			if (str[index - 1] == ' ' ||
+			    str[index - 1] == '\t' ||
+			    str[index - 1] == '\n' ||
+			    str[index - 1] == ',' ||
+			    str[index - 1] == ';' ||
+			    str[index - 1] == '.' ||
+			    str[index - 1] == '!' ||
+			    str[index - 1] == '?' ||
+			    str[index - 1] == '"' ||
+			    str[index - 1] == '(' ||
+			    str[index - 1] == ')' ||
+			    str[index - 1] == '{' ||
+			    str[index - 1] == '}' ||
+			     index == 0)
+				str[index] -= 32;
+
+			index++;
 		}
-	}
+
+	return (str);
 }
+
